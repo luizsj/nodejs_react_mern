314247b881b7c06693dc33456c3f52263ccde9a2
LuizSoares <luiz.jesus@fatec.sp.gov.br>
2021-12-09 22:50:27 -0300

    a.02- esqueleto do servidor express

--- server.js
+++ server.js
@@ -1,1 +1,19 @@
-console.log('hello world')
+const express = require('express')
+const cookieParser = require('cookie-parser')
+const cors = require('cors')
+const path = require('path')
+
+const app = express();
+const port = process.env.PORT || 8080
+app.use(cors());
+
+app.use(cookieParser());
+app.use(express.json());
+
+app.get('/', (req, res) => {
+    res.json({message: "hello world"});
+});
+
+app.listen(port, () => {
+    console.log('server running on port '+port)
+});
