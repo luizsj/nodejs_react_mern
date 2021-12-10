3f8eeeaedf0e505fa994a89f994f8de4b3a33245
LuizSoares <luiz.jesus@fatec.sp.gov.br>
2021-12-09 23:20:22 -0300

    a.04 - conexão mongodb

--- server.js
+++ server.js
@@ -1,10 +1,23 @@
-const express = require('express')
-const cookieParser = require('cookie-parser')
-const cors = require('cors')
-const path = require('path')
+const express = require('express');
+const cookieParser = require('cookie-parser');
+const cors = require('cors');
+const path = require('path');
+const mongoose = require('mongoose');
 
 const app = express();
 const port = process.env.PORT || 8080
+
+    //Mongoose
+    mongoose.Promise = global.Promise;
+    mongoose.connect('mongodb://localhost/curso_mern', {
+        useNewUrlParser: true , 
+        useUnifiedTopology: true
+    }).then(() => {
+        console.log(' conectado banco de dados')
+    }).catch((err) => {
+        console.log(' erro banco de dados: '+err)
+    });
+
 app.use(cors());
 
 app.use(cookieParser());
