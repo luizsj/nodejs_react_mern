f77db9a76c3a57fecb2181f52e51f1b589ab3357
LuizSoares <luiz.jesus@fatec.sp.gov.br>
2021-12-12 16:39:17 -0300

    a.10: start client react

---
+++ client/src/App.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <div className="App-header">
+          <h2>CURSO BÁSICO NODEJS COM REACTJS</h2>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default App;
