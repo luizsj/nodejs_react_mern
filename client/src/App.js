775b8c4055368ca4826e717bd61492a8acf1a647
LuizSoares <luiz.jesus@fatec.sp.gov.br>
2021-12-12 16:57:36 -0300

    a.12: install material-ui, import album layout

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,13 @@
 import React, { Component } from 'react';
-
+import Inicio from './pages/client/painel';
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <div className="App-header">
+        {/*<div className="App-header">
           <h2>CURSO BÁSICO NODEJS COM REACTJS</h2>
-        </div>
+    </div>*/}
+    <Inicio/>
       </div>
     );
   }
