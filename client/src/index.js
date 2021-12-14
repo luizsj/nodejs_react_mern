3929844c15e65e8a781a6338e6414961fc6819ad
LuizSoares <luiz.jesus@fatec.sp.gov.br>
2021-12-14 20:45:15 -0300

    a.14: starting pages react: bug de versao de pacotes, reiniciar projeto

--- client/src/index.js
+++ client/src/index.js
@@ -2,9 +2,23 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 
+import { StyledEngineProvider, ThemeProvider, createTheme  } from "@mui/material/styles";
+import { red } from '@mui/material/colors';
+
+const theme = createTheme({
+  palette: {
+    primary: {
+      main: red[500],
+    },
+  },
+});
+
 ReactDOM.render(
-  
+  <StyledEngineProvider injectFirst> 
+  <ThemeProvider theme={theme}>
   <App />
+  </ThemeProvider>
+      </StyledEngineProvider>
   ,
   document.getElementById('root')
 );
